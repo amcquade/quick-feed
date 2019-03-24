71dc2b4d22c11f852c92fe5fdba050ab5032999d
Aaron McQuade <am047613@ohio.edu>
2019-03-24 14:49:19 -0400

    Updates

---
+++ precache-manifest.903c1569b62252d270219f7ebbd2e33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b068a0ac6cd38e52c6b59476c9f4cb6",
+    "url": "/quick-feed/build/index.html/static/media/logo.0b068a0a.svg"
+  },
+  {
+    "revision": "b34b2c02af8ca5f8d419",
+    "url": "/quick-feed/build/index.html/static/js/runtime~main.b34b2c02.js"
+  },
+  {
+    "revision": "f6fe6f428d382390cae8",
+    "url": "/quick-feed/build/index.html/static/js/main.f6fe6f42.chunk.js"
+  },
+  {
+    "revision": "a5a15987030af597fde5",
+    "url": "/quick-feed/build/index.html/static/js/1.a5a15987.chunk.js"
+  },
+  {
+    "revision": "f6fe6f428d382390cae8",
+    "url": "/quick-feed/build/index.html/static/css/main.9bce0606.chunk.css"
+  },
+  {
+    "revision": "9651bc0ffc7dddeb3bcd0c5506bd9b78",
+    "url": "/quick-feed/build/index.html/index.html"
+  }
+];
