b8ac9bea5b6449b85b17b11467d930e87b4a242c
Aaron McQuade <am047613@ohio.edu>
2019-03-24 13:43:01 -0400

    added material ui dialog to alert user if there is an error in parsering the feed url

--- src/components/AlertDialog.jsx
+++ src/components/AlertDialog.jsx
@@ -6,49 +6,27 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-class AlertDialog extends React.Component {
-  state = {
-    open: true
-  };
-
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    return (
-      <div>
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            {"Use Google's location service?"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Let Google help apps determine location. This means sending
-              anonymous location data to Google, even when no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Disagree
-            </Button>
-            <Button onClick={this.handleClose} color="primary" autoFocus>
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
-  }
-}
-
-export default AlertDialog;
+export default ({ open, msg, msg_type, handleClose }) => {
+  return (
+    <div>
+      <Dialog
+        open={open}
+        // onClose={this.props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{msg_type}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {msg}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
--- src/components/EpisodeList.jsx
+++ src/components/EpisodeList.jsx
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
 import Episode from "./Episode";
-import UserForm from "./UserForm";
-import logo from "../logo.svg";
-import AlertDialog from "./AlertDialog";
 
 class EpisodeList extends Component {
-  state = {
-    episodes: null,
-    program_image: null,
-    fetching: false,
-    program_title: null,
-    program_description: null
-  };
-
-  renderEpisodes = () => {
+  renderEpisodeList = () => {
     return (
       <div>
         <h1>
-          {this.state.program_title} by {this.state.creator}
+          {this.props.program_title} by {this.props.creator}
         </h1>
-        <img src={this.state.program_image} />
-        <p>{this.state.program_description}</p>
-        {this.state.episodes.map(this.returnEpisodes)}
+        <img src={this.props.program_image} />
+        <p>{this.props.program_description}</p>
+        {this.props.episodes.map(this.returnEpisodes)}
       </div>
     );
   };
@@ -38,83 +27,15 @@ class EpisodeList extends Component {
             ? episode.enclosure.url
             : "json_data is null or undefined"
         }
-        image={this.state.program_image}
+        image={this.props.program_image}
         description={episode.description}
       />
     );
   };
 
-  getFeed = e => {
-    this.setState({ fetching: !this.state.fetching });
-    e.preventDefault();
-    const feed_url = e.target.elements.feed_url.value;
-    let Parser = require("rss-parser");
-    let parser = new Parser({
-      customFields: {
-        item: [["enclosure", { keepArray: true }]]
-      }
-    });
-    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-
-    if (feed_url) {
-      var arr = [];
-      // parser.parseURL(CORS_PROXY + feed_url, function(err, feed) {
-
-      //   feed.items.forEach(function(entry) {
-      //     arr.push(entry);
-      //   });
-      // });
-      // console.log(arr);
-      // this.setState({
-      //   episodes: arr,
-      //   program_title: "feed.title",
-      //   fetching: !this.state.fetching,
-      //   program_image: "feed.image.url",
-      //   program_description: "feed.description"
-      // });
-      (async () => {
-        try {
-          let feed = await parser.parseURL(CORS_PROXY + feed_url);
-          let arr = [];
-          console.log("feed: " + feed);
-          feed.items.forEach(item => {
-            arr.push(item);
-          });
-          this.setState({
-            episodes: arr,
-            program_title: feed.title,
-            fetching: !this.state.fetching,
-            program_image: feed.image.url,
-            program_description: feed.description
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      })();
-    } else {
-      return;
-    }
-  };
-
   render() {
     return (
-      <div>
-        <UserForm getFeed={this.getFeed} />
-        {this.state.episodes ? (
-          this.renderEpisodes()
-        ) : (
-          <div>
-            {!this.state.fetching ? (
-              <p>Please enter an RSS feed</p>
-            ) : (
-              <div>
-                <img src={logo} className="App-logo" />
-                {!this.state.error ? <AlertDialog /> : <div />}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
+      <div>{this.props.episodes ? this.renderEpisodeList() : <div />}</div>
     );
   }
 }
