c1bca09f5dad7e17fcae05fa91899c05078380c2
Aaditya Chakravarty <ac36500@gmail.com>
2019-10-06 20:11:05 +0530

    Added Bootstrap and fixed some typos.

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 
 class Episode extends Component {
+  divStyles = {
+    width: "77vw",
+    float: "right",
+    "margin-right": "1vw"
+  }
   render() {
     return (
-      <div>
-        {this.props.title}
-        <a href={this.props.link}>{this.props.title}</a>
-        <p>{this.props.description}</p>
+      <div class="list-group" style={this.divStyles}>
+        <a href={this.props.link} className="list-group-item list-group-item-action text-left">{this.props.title}</a>
       </div>
     );
   }
