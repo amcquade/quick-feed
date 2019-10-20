d36e63b94a7b3a7a56a166bc526712d23a17eaa3
aaron <am047613@ohio.edu>
2019-10-19 21:51:25 -0400

    Updates

---
+++ precache-manifest.afcfc4824837fddebf5d1c9b3d543b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422097f49ab6f2fcabeb90a6b81af80d",
+    "url": "/quick-feed/index.html"
+  },
+  {
+    "revision": "4c703e56b96af319c06b",
+    "url": "/quick-feed/static/css/main.aa0d1aaa.chunk.css"
+  },
+  {
+    "revision": "73f88b55461ae994f0b8",
+    "url": "/quick-feed/static/js/2.3443ed56.chunk.js"
+  },
+  {
+    "revision": "4c703e56b96af319c06b",
+    "url": "/quick-feed/static/js/main.e2a1a0a0.chunk.js"
+  },
+  {
+    "revision": "f89c690cd1cc23477d25",
+    "url": "/quick-feed/static/js/runtime-main.1e4e871c.js"
+  },
+  {
+    "revision": "0b068a0ac6cd38e52c6b59476c9f4cb6",
+    "url": "/quick-feed/static/media/logo.0b068a0a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quick-feed/precache-manifest.05629aacdb4ccd5effb844375e4ead93.js"
+  "/quick-feed/precache-manifest.afcfc4824837fddebf5d1c9b3d543b92.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/quick-feed/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quick-feed/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
