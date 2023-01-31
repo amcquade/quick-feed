3c97afaf4f280744049c298c12e750a86a5ae918
salamisodikiolawale <salamisodikiolawale@gmail.com>
2023-01-31 15:36:09 +0100

    refactor the class component into function component for consistency

--- src/components/EpisodeList.jsx
+++ src/components/EpisodeList.jsx
@@ -1,72 +1,69 @@
-import React, { Component } from "react";
+import React from "react";
 import FavoriteButton from "./FavoriteButton";
 import Episode from "./Episode";
+import { useState } from "react";
 
 
-class EpisodeList extends Component {
-  cardStyle = {
-    width: "20vw",
-    float: "left",
-  };
+const  EpisodeList = ({
+    program_title,
+    program_description,
+    program_image,
+    episodes,
+  }) => {
+  
+  
+  // eslint-disable-next-line no-unused-vars
+  const [cardStyle, setCardStyle] = useState({width: "20vw", float: "left"});
 
-  render() {
-    const {
-      program_title,
-      program_description,
-      program_image,
-      episodes,
-    } = this.props;
-
-    const toggleFavorite = () => {
-      alert('test');
-    }
-
-    return (
-      <div>
-        {episodes ? (
-          <div>
-            <div id="menu" styles={{ float: "left" }}></div>
-            <div className="card" style={this.cardStyle}>
-              <img
-                src={program_image}
-                className="card-img-top"
-                alt={program_title}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{program_title}</h5>
-                <FavoriteButton 
-                  selected={true}
-                  onClickAction={toggleFavorite} />
-                <div
-                  className="card-text"
-                  dangerouslySetInnerHTML={{
-                    __html: program_description,
-                  }}
-                />
-              </div>
-            </div>
-            {episodes.map((episode, i) => (
-              <Episode
-                key={Math.random() * i}
-                index={i}
-                title={episode.title}
-                enclosure={episode.enclosure}
-                link={
-                  episode.enclosure
-                    ? episode.enclosure.url
-                    : "json_data is null or undefined"
-                }
-                image={program_image}
-                description={episode.description}
-              />
-            ))}
-          </div>
-        ) : (
-          <div />
-        )}
-      </div>
-    );
+  const toggleFavorite = () => {
+    alert('test');
   }
+
+  return (
+    <div>
+      {episodes ? (
+        <div>
+          <div id="menu" styles={{ float: "left" }}></div>
+          <div className="card" style={cardStyle}>
+            <img
+              src={program_image}
+              className="card-img-top"
+              alt={program_title}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{program_title}</h5>
+              <FavoriteButton 
+                selected={true}
+                onClickAction={toggleFavorite} />
+              <div
+                className="card-text"
+                dangerouslySetInnerHTML={{
+                  __html: program_description,
+                }}
+              />
+            </div>
+          </div>
+          {episodes.map((episode, i) => (
+            <Episode
+              key={Math.random() * i}
+              index={i}
+              title={episode.title}
+              enclosure={episode.enclosure}
+              link={
+                episode.enclosure
+                  ? episode.enclosure.url
+                  : "json_data is null or undefined"
+              }
+              image={program_image}
+              description={episode.description}
+            />
+          ))}
+        </div>
+      ) : (
+        <div />
+      )}
+    </div>
+  );
 }
 
 export default EpisodeList;
